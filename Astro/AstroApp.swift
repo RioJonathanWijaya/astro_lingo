b5d15b3c0ba77f70cf1e8795e19dc533739e8836
riojonathan <c14210006@john.petra.ac.id>
2024-04-26 14:47:51 +0700

    Initial Commit

---
+++ Astro/AstroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroApp.swift
+//  Astro
+//
+//  Created by Rio Jonathan on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AstroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Astro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Astro
+//
+//  Created by Rio Jonathan on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
